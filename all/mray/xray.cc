26264c718d587158010acd040ea3bcf1dcf0f3ab
pc-odroid <semoga@gmx.net>
2026-01-04 13:37:49 +0100

    Plattkarten zugeschnitten

--- all/mray/xray.cc
+++ all/mray/xray.cc
@@ -164,16 +164,16 @@ void addkugel (cwelt& pwelt)
   //ckoerper* koerper1= new ckoerper (new cskugel, new cparakugelw, new cbegrkeine, new cscreenmannigz (new cjpegdatei ("/root/quell-cc/media/Mercatorkarten/2054 Mercator-blza physisch hellblau.jpg"), real (1)/PI, 0), kstobj, klobj);
   //ckoerper* koerper1= new ckoerper (new cskugel, new cparakugelw, new cbegrkeine, new cscreenmannigz (new cjpegdatei ("/root/quell-cc/media/Mercatorkarten/2400 Mercator-blza physisch dunkelblau.jpg"), real (1)/PI, 0), kstobj, klobj);
   //ckoerper* koerper1= new ckoerper (new cskugel, new cparakugelw, new cbegrkeine, new cscreenmannigz (new cjpegdatei ("/root/quell-cc/media/Mercatorkarten/2768 Mercator-bl85 braun.jpg"), real (1)/PI, 0), kstobj, klobj);
-  ckoerper* koerper1= new ckoerper (new cskugel, new cparakugelw, new cbegrkeine, new cscreenmannigz (new cjpegdatei ("/root/quell-cc/media/Mercatorkarten/3821 Mercator-blz politisch.jpg"), real (1)/PI, 0), kstobj, klobj);
+  //ckoerper* koerper1= new ckoerper (new cskugel, new cparakugelw, new cbegrkeine, new cscreenmannigz (new cjpegdatei ("/root/quell-cc/media/Mercatorkarten/3821 Mercator-blz politisch.jpg"), real (1)/PI, 0), kstobj, klobj);
   //ckoerper* koerper1= new ckoerper (new cskugel, new cparakugelw, new cbegrkeine, new cscreenmannigz (new cjpegdatei ("/root/quell-cc/media/Mercatorkarten/6732 Mercator-blz Straßennetz.jpg"), real (1)/PI, 0), kstobj, klobj);
   //ckoerper* koerper1= new ckoerper (new cskugel, new cparakugelw, new cbegrkeine, new cscreenmannigz (new cjpegdatei ("/root/quell-cc/media/Mercatorkarten/7682 Mercator-blz politisch.jpg"), real (1)/PI, 0), kstobj, klobj);
 
   // Plattkarten
   //ckoerper* koerper1= new ckoerper (new cskugel, new cparakugel, new cbegrkeine, new cscreenmannigz (new cjpegdatei ("/root/quell-cc/media/Plattkarten/0400 platt hellblau.jpg"), real (1)/PI, 0), kstobj, klobj);
   //ckoerper* koerper1= new ckoerper (new cskugel, new cparakugel, new cbegrkeine, new cscreenmannigz (new cjpegdatei ("/root/quell-cc/media/Plattkarten/1000 platt dunkelblau.jpg"), real (1)/PI, 0), kstobj, klobj);
-  //ckoerper* koerper1= new ckoerper (new cskugel, new cparakugel, new cbegrkeine, new cscreenmannigz (new cjpegdatei ("/root/quell-cc/media/Plattkarten/1200 platt politisch.jpg"), real (1)/PI, 0), kstobj, klobj);
   //ckoerper* koerper1= new ckoerper (new cskugel, new cparakugel, new cbegrkeine, new cscreenmannigz (new cjpegdatei ("/root/quell-cc/media/Plattkarten/1200 platt schief.jpg"), real (1)/PI, 0), kstobj, klobj);
-  //ckoerper* koerper1= new ckoerper (new cskugel, new cparakugel, new cbegrkeine, new cscreenmannigz (new cjpegdatei ("/root/quell-cc/media/Plattkarten/2058 platt physisch.jpg"), real (1)/PI, 0), kstobj, klobj);
+  //ckoerper* koerper1= new ckoerper (new cskugel, new cparakugel, new cbegrkeine, new cscreenmannigz (new cjpegdatei ("/root/quell-cc/media/Plattkarten/1200 platt politisch.jpg"), real (1)/PI, 0), kstobj, klobj);
+  ckoerper* koerper1= new ckoerper (new cskugel, new cparakugel, new cbegrkeine, new cscreenmannigz (new cjpegdatei ("/root/quell-cc/media/Plattkarten/2044 platt-z physisch.jpg"), real (1)/PI, 0), kstobj, klobj);
 
   // Körper addieren
   pwelt.addkoerper (koerper1);
@@ -216,9 +216,10 @@ void addtorus (cwelt& pwelt)
   //ckoerper* koerper1= new ckoerper (new cstorus (real (0.125)), new cparatorus, new cbegrkeine, new cscreenmannig (new cjpegdatei ("/root/quell-cc/media/Texturen/Palmen.jpg"), real (16/PI), real (2/PI)), kstobj, kl1);
   //ckoerper* koerper1= new ckoerper (new cstorus (real (0.125)), new cparatorus, new cbegrkeine, new cscreenmannig (new cjpegdatei ("/root/quell-cc/media/Texturen/Steine.jpg"), real (16/PI), real (2/PI)), kstobj, kl1);
   //ckoerper* koerper1= new ckoerper (new cstorus (real (0.125)), new cparatorus, new cbegrkeine, new cscreenmannig (new cjpegdatei ("/root/quell-cc/media/Texturen/Steinmauer.jpg"), real (16/PI), real (2/PI)), kstobj, kl1);
-  ckoerper* koerper1= new ckoerper (new cstorus (real (0.125)), new cparatorusu, new cbegrkeine, new cscreenmannig (new cjpegdatei ("/root/quell-cc/media/Texturen/Steinstrand.jpg"), real (16/PI), real (2/PI)), kstobj, klobj);
+  //ckoerper* koerper1= new ckoerper (new cstorus (real (0.125)), new cparatorusu, new cbegrkeine, new cscreenmannig (new cjpegdatei ("/root/quell-cc/media/Texturen/Steinstrand.jpg"), real (16/PI), real (2/PI)), kstobj, klobj);
   //ckoerper* koerper1= new ckoerper (new cstorus (real (0.125)), new cparatorusu, new cbegrkeine, new cscreenmannig (new cjpegdatei ("/root/quell-cc/media/Texturen/Steinstrand.jpg"), real (0.5/PI), real (0.5/PI)), kstobj, klobj);
-  //ckoerper* koerper1= new ckoerper (new cstorus (real (40)), new cparatoruss, new cbegrkeine, new cscreenmannigz (new cjpegdatei ("/root/quell-cc/media/Karten/Mercator symmetrisch.jpg"), real (1/PI), real (2/PI)), kstobj, klobj);
+  ckoerper* koerper1= new ckoerper (new cstorus (real (0.125)), new cparatorusu, new cbegrkeine, new cscreenmannig (new cjpegdatei ("/root/quell-cc/media/Plattkarten/2044 platt-z physisch.jpg"), real (16/PI), real (1/PI)), kstobj, klobj);
+  //ckoerper* koerper1= new ckoerper (new cstorus (real (40)), new cparatoruss, new cbegrkeine, new cscreenmannigz (new cjpegdatei ("/root/quell-cc/media/Plattkarten/2044 platt-z physisch.jpg"), real (1/PI), real (2/PI)), kstobj, klobj);
 
   // Körper addieren
   pwelt.addkoerper (koerper1);
@@ -285,8 +286,8 @@ void fliege ()
   //addkoord (*welt);
   //addebenen90 (*welt);
   //addzylinder (*welt);
-  addkugel (*welt);
-  //addtorus (*welt);
+  //addkugel (*welt);
+  addtorus (*welt);
 
   //cflugsimu flugsimu (welt, new cxkeyboard, new cxscreen ("xray", 800, 450), 450);                    // cxscreen: Fehlerpixel mit xraythread
 
@@ -294,7 +295,8 @@ void fliege ()
   //cflugsimu flugsimu (welt, new cxkeyboard, new cximagescreen ("xray", 512, 256), 512);
   //cflugsimu flugsimu (welt, new cxkeyboard, new cximagescreen ("xray", 1024, 512), 1024);
   //cflugsimu flugsimu (welt, new cxkeyboard, new cximagescreen ("xray", 750, 500), 750);
-  cflugsimu flugsimu (welt, new cxkeyboard, new cximagescreen ("xray", 1500, 1000), 1500);
+  cflugsimu flugsimu (welt, new cxkeyboard, new cximagescreen ("xray", 1350, 1000), 1350);
+  //cflugsimu flugsimu (welt, new cxkeyboard, new cximagescreen ("xray", 1500, 1000), 1500);
   //cflugsimu flugsimu (welt, new cxkeyboard, new cximagescreen ("xray", 1920, 1080), 1920);
 
   //cflugsimu flugsimu (welt, new cnckeyboard, new cfbscreen ("xray", 512, 256), 512);
---
+++ cc/single/gradnetz.cc
@@ -0,0 +1,138 @@
+#include "../../cc/screen/jpegscr.h"
+#include "../../cc/screen/bmpscr.h"
+
+void gradl (clscreen8* pscreen)
+  {
+  real farbe;
+
+  // Längengrade zeichnen
+  real lmitte= real (pscreen->xanz-1)/2;
+  for (integer laufx= -36; laufx <= 36; laufx++)
+    {
+    real lg= real (pscreen->xanz*laufx)/72;
+    real putx= lmitte + lg;
+    farbe= (putx - floorr (putx))*255;
+    integer putxi= integer (floorr (putx));
+
+    for (integer laufy= 0; laufy < pscreen->yanz; laufy++)
+      pscreen->putpixel (putxi, laufy, integer (farbe), integer (farbe), integer (farbe));
+    if (farbe != 0)
+      for (integer laufy= 0; laufy < pscreen->yanz; laufy++)
+        pscreen->putpixel (putxi+1,laufy, 255-integer (farbe), 255-integer (farbe), 255-integer (farbe));
+
+    // Nullmeridian zeichnen
+    if (laufx == 0)
+      {
+      for (integer laufy= 0; laufy < pscreen->yanz; laufy++)
+        pscreen->putpixel (putxi, laufy, integer (255-farbe), 0, 0);
+      if (farbe != 0)
+        for (integer laufy= 0; laufy < pscreen->yanz; laufy++)
+          pscreen->putpixel (putxi+1,laufy, integer (farbe), 0, 0);
+      }
+
+    // -180° Meridian zeichnen
+    if (laufx == -36)
+      {
+      for (integer laufy= 0; laufy < pscreen->yanz; laufy++)
+        pscreen->putpixel (putxi, laufy, 0, integer (255-farbe), 0);
+      if (farbe != 0)
+        for (integer laufy= 0; laufy < pscreen->yanz; laufy++)
+          pscreen->putpixel (putxi+1,laufy, 0, integer (farbe), 0);
+      }
+
+    // 180° Meridian zeichnen
+    if (laufx == 36)
+      {
+      for (integer laufy= 0; laufy < pscreen->yanz; laufy++)
+        pscreen->putpixel (putxi, laufy, 0, 0, integer (255-farbe));
+      if (farbe != 0)
+        for (integer laufy= 0; laufy < pscreen->yanz; laufy++)
+          pscreen->putpixel (putxi+1,laufy, 0, 0, integer (farbe));
+      }
+    }
+  }
+
+void gradbmercator (clscreen8* pscreen)
+  {
+  real farbe;
+
+  // Breitengrade zeichnen
+  real pxrad= PI2/real (pscreen->xanz);
+  real bmitte= real (pscreen->yanz-1)/2;
+  for (integer laufy= -17; laufy < 18; laufy++)
+    {
+    real bg= atanhr (sinr (real (laufy)/36*PI))/pxrad;
+    real puty= bmitte + bg;
+    farbe= (puty - floorr (puty))*255;
+    integer putyi= integer (floorr (puty));
+
+    for (integer laufx= 0; laufx < pscreen->xanz; laufx++)
+      pscreen->putpixel (laufx, putyi, integer (farbe), integer (farbe), integer (farbe));
+    if (farbe != 0)
+      for (integer laufx= 0; laufx < pscreen->xanz; laufx++)
+        pscreen->putpixel (laufx, putyi+1, 255 - integer (farbe), 255-integer (farbe), 255-integer (farbe));
+
+    // Äquator zeichnen
+    if (laufy == 0)
+      {
+      for (integer laufx= 0; laufx < pscreen->xanz; laufx++)
+        pscreen->putpixel (laufx, putyi, 255 - integer (farbe), 0, 0);
+      if (farbe != 0)
+        for (integer laufx= 0; laufx < pscreen->xanz; laufx++)
+          pscreen->putpixel (laufx, putyi+1, integer (farbe), 0, 0);
+      }
+    }
+  }
+
+void gradbplatt (clscreen8* pscreen)
+  {
+  real farbe;
+
+  // Breitengrade zeichnen
+  real pxrad= PI2/real (pscreen->xanz);
+  real bmitte= real (pscreen->yanz-1)/2;
+  for (integer laufy= -17; laufy < 18; laufy++)
+    {
+    real bg= real (laufy)/36*PI/pxrad;
+    real puty= bmitte + bg;
+    farbe= (puty - floorr (puty))*255;
+    integer putyi= integer (floorr (puty));
+
+    for (integer laufx= 0; laufx < pscreen->xanz; laufx++)
+      pscreen->putpixel (laufx, putyi, integer (farbe), integer (farbe), integer (farbe));
+    if (farbe != 0)
+      for (integer laufx= 0; laufx < pscreen->xanz; laufx++)
+        pscreen->putpixel (laufx, putyi+1, 255 - integer (farbe), 255-integer (farbe), 255-integer (farbe));
+
+    // Äquator zeichnen
+    if (laufy == 0)
+      {
+      for (integer laufx= 0; laufx < pscreen->xanz; laufx++)
+        pscreen->putpixel (laufx, putyi, 255 - integer (farbe), 0, 0);
+      if (farbe != 0)
+        for (integer laufx= 0; laufx < pscreen->xanz; laufx++)
+          pscreen->putpixel (laufx, putyi+1, integer (farbe), 0, 0);
+      }
+    }
+  }
+
+void zeichnegradnetz (char* p_name)
+  {
+  // jpeg-Datei einlesen und in eine bmp-Datei speichern
+  cjpegdatei jpgdatei (p_name);
+  cbmpdatei bmpdatei ("mercator.bmp", jpgdatei.xanz, jpgdatei.yanz);
+  bmpdatei.putscreen (jpgdatei, 0, 0);
+
+  // Gradnetz in bmp-Datei zeichnen
+  gradl (&bmpdatei);
+  //gradbmercator (&bmpdatei);
+  gradbplatt (&bmpdatei);
+  }
+
+int main (int argc, char** argv)
+  {
+  if (argc != 2)
+    return 0;
+  zeichnegradnetz (argv[1]);
+  return 0;
+  }
